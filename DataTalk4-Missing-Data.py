50d6832cc06da6eec26b89d89f16e373b664c373
angelmaysmile <tminhkk93@gmail.com>
2018-08-11 13:49:27 +0700

    Update missing data files

--- DataTalk4-Missing-Data.py
+++ DataTalk4-Missing-Data.py
@@ -7,34 +7,43 @@ score_hg = pd.read_csv("Examination Result 2017/diem_thi_ha_giang.csv")
 score_hg.head()
 
 print("Shape of data:", score_hg.shape)
-score_hg.describe()
+print(score_hg.describe())
 
 # Missing at Random (MAR): propensity for a data point to be missing is not related to the missing data,but it is related to some of the observed data
 # Missing Completely at Random (MCAR): certain value is missing has nothing to do with its hypothesis value and with the values of other variables
 # Missing not at Random (MNAR): missing value depends on the hypothetical value
 
-# MAR and MCAR: it's safe to delete the data
-# Deletion
+# Remove all rows with missing maths score
 score_hg_drop_math = score_hg[pd.isnull(score_hg['TOAN']) == False]
-print(score_hg_drop_math.shape)
+print("Length of the data now:", len(score_hg_drop_math))
 # Dropping variable
 score_hg_drop_hist = score_hg.drop('LICH SU', axis=1)
-score_hg_drop_hist.head()
+print("List of columns now:", score_hg_drop_hist.columns)
 
-# MNAR: it's not allowed to delete the missing data
-# Linear regression: missing data - dependent variables. Independent variables can be the score of other subjects
-# Multiple imputation: impute data from a distribution to the missing datas and consolidate the results
-# K nearest neighbors: requires a distance metric and selection of the number of nearest neighbors to impute the data
-# Mean, Median and Mode
-score_hg_math = score_hg
-score_math = score_hg_math.TOAN
+# Impute missing data with mean
+score_math = score_hg.TOAN
 imputer = Imputer(missing_values=float('NaN'), strategy='mean')
 # imputer = Imputer(missing_values=float('NaN'), strategy='median')
 # imputer = Imputer(missing_values=float('NaN'), strategy='most_frequent')
 score_math = score_math.values.reshape(-1, 1)
+# Transform the data with filled value for missing data
 transformed_score_math = imputer.fit_transform(score_math)
-score_hg_math.TOAN = pd.Series(transformed_score_math.tolist())
-score_hg_math.TOAN = score_hg_math.TOAN.apply(lambda x: list(x)[0])
+score_hg.TOAN = pd.Series(transformed_score_math.tolist())
+score_hg.TOAN = score_hg.TOAN.apply(lambda x: list(x)[0])
 
-print(score_hg_math_mean.describe())
+print(score_hg.describe())
 
+# First quantile
+Q1 = score_hg['GDCD'].quantile(0.25)
+# Third quantile
+Q3 = score_hg['GDCD'].quantile(0.75)
+# Interquantile Range
+IQR = Q3 - Q1
+score_hg = score_hg[(score_hg['GDCD']<=Q3+1.5*IQR) & (score_hg['GDCD']>=Q1-1.5*IQR)]
+
+print(score_hg.describe())
+
+# Remove all duplicates in the data set
+score_hg.drop_duplicates(keep="first", inplace=True)
+
+print(score_hg.describe())
